71b555ddeb5b73193a591be663c0b8dcf3b77f87
Bruno Paulin <brunopaulin@bpaulin.net>
2014-01-22 17:40:37 +0100

    plexterm: handle wrong url

--- plexterm.py
+++ plexterm.py
@@ -6,24 +6,29 @@ import subprocess
 import argparse
 
 parser = argparse.ArgumentParser(description='Open plex media')
-parser.add_argument('--server', metavar='SERVER', type=str, action="store", help='serven ip', default='http://192.168.1.100:32400')
+parser.add_argument('--server', metavar='SERVER', type=str, action="store", help='server ip', default='http://localhost:32400')
 parser.add_argument('id', metavar='ID', type=str, action="store", help='media id')
 args = vars(parser.parse_args())
 server = args['server']
 id = args['id']
 
-metadata = urllib.urlopen(server+'/library/metadata/'+str(id)).read()
-tree = ET.fromstring(metadata)
-video = tree.find("./Video/Media/Part").get('key')
-argsvlc = [
-    "vlc",
-    "--qt-minimal-view",
-    server+video
-]
 try:
-    sub = tree.find("./Video/Media/Part/Stream[@format='srt']").get('key')
-    argsvlc.append("--sub-file="+server+sub)
+    url = server+'/library/metadata/'+str(id)
+    metadata = urllib.urlopen(url).read()
 except Exception, e:
-    print "no subs"
-subprocess.call(argsvlc)
-print "Hello, World!"
+    print "Can't read from "+url
+else:
+    tree = ET.fromstring(metadata)
+    video = tree.find("./Video/Media/Part").get('key')
+    argsvlc = [
+        "vlc",
+        "--qt-minimal-view",
+        server+video
+    ]
+    try:
+        sub = tree.find("./Video/Media/Part/Stream[@format='srt']").get('key')
+        argsvlc.append("--sub-file="+server+sub)
+    except Exception, e:
+        print "no subs"
+    subprocess.call(argsvlc)
+    print "Hello, World!"
